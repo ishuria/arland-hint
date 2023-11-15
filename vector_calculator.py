0df6d8f76e2a4f9e4d1aa5303f5947b4a63d1472
xiangchaolei <xiangchaolei@bytedance.com>
2023-11-14 17:04:28 -0800

    1

--- vector_calculator.py
+++ vector_calculator.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
     with open('/home/len/information-hint-data/class.txt', 'r') as f:
         print("["+",".join(f.readlines())+"]")
-        knowledges = json.loads("["+",".join(f.readlines())+"]")
+        knowledges = json.loads("[\""+"\",\"".join(f.readlines())+"\"]")
     
     it_item_index_id_mapping = get_it_item_index_id_mapping()
     # print(it_item_index_id_mapping)
