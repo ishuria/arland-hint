7a6460074bf3b27137d4d55744ab6ed15e665fde
xiangchaolei <xiangchaolei@bytedance.com>
2023-11-16 15:11:35 -0800

    1

--- gen_answer.py
+++ gen_answer.py
@@ -5,7 +5,7 @@ import signal
 from transformers import AutoTokenizer, AutoModel
 import readline
 from util.db_util import get_it_item_index_id_mapping
-from util.file_util import read_file_content_as_string, index_to_path
+from util.file_util import read_file_content_as_string, read_file_content
 from util.config_util import DATA_FOLDER, CHATGLM_6B_FOLDER
 
 tokenizer = AutoTokenizer.from_pretrained(CHATGLM_6B_FOLDER, trust_remote_code=True)
@@ -15,105 +15,6 @@ model = model.eval()
 os_name = platform.system()
 clear_command = 'cls' if os_name == 'Windows' else 'clear'
 stop_stream = False
-
-
-def build_prompt(history):
-    prompt = "欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序"
-    for query, response in history:
-        prompt += f"\n\n用户：{query}"
-        prompt += f"\n\nChatGLM-6B：{response}"
-    return prompt
-
-
-def signal_handler(signal, frame):
-    global stop_stream
-    stop_stream = True
-
-
-def list_to_file(file_name: str, content: list):
-    with open(file_name, 'w', encoding="utf-8") as f:
-        for s in content:
-            f.write(s + '\n')
-
-
-train_datas = []
-with open('/home/len/information-hint-data/kancd-data.txt', 'r') as f:
-    train_datas = [line.rstrip('\n') for line in f]
-
-knowledge_index_map = {}
-with open('/home/len/information-hint-data/class.txt', 'r') as f:
-    knowledge_list = [line.rstrip('\n') for line in f]
-    for i in range(len(knowledge_list)):
-        knowledge_index_map[knowledge_list[i]] = i + 1
-
-
-def main():
-    history = []
-    global stop_stream
-    print("欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序")
-    while True:
-        query = input("\n用户：")
-        if query.strip() == "stop":
-            break
-        if query.strip() == "clear":
-            history = []
-            os.system(clear_command)
-            print("欢迎使用 ChatGLM-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序")
-            continue
-        count = 0
-        for response, history in model.stream_chat(tokenizer, query, history=history):
-            if stop_stream:
-                stop_stream = False
-                break
-            else:
-                count += 1
-                if count % 8 == 0:
-                    os.system(clear_command)
-                    #            print(build_prompt(history), flush=True)
-                    signal.signal(signal.SIGINT, signal_handler)
-        os.system(clear_command)
-        print(build_prompt(history), flush=True)
-
-
-# 每10万个记录形成一个文件夹
-# levelOne/levelTwo/levelThree/dividend
-def index_to_path(item_index: int):
-    levelOne = 0
-    levelTwo = 0
-    levelThree = 0
-    dividend = item_index % 100;
-    item_index //= 100
-    levelThree = item_index % 100;
-    item_index //= 100
-    levelTwo = item_index % 100;
-    item_index //= 100
-    levelOne = item_index % 100;
-    item_index //= 100
-    return str(levelOne) + os.sep + str(levelTwo) + os.sep + str(levelThree) + os.sep + str(dividend)
-
-
-def read_file_content(item_index: int, categories: list, no_sep: bool):
-    list = []
-    for category in categories:
-        _list = []
-        if not os.path.isfile(DATA_FOLDER + index_to_path(item_index) + os.sep + category + '.txt'):
-            continue
-        with open(DATA_FOLDER + index_to_path(item_index) + os.sep + category + '.txt', 'r', encoding='utf-8') as f:
-            _list = [line.rstrip('\n') for line in f]
-        list.extend(_list)
-        if not no_sep:
-            list.append('<sep>')
-    return list
-
-new_hint_map = {}
-
-new_hint_list = []
-with open('/home/len/kancd-data/new_hint.txt', 'r', encoding='utf-8') as f:
-    new_hint_list = [line.rstrip('\n') for line in f]
-
-for new_hint in new_hint_list:
-    id, h = new_hint.split('\t')
-    new_hint_map[id]=h
 
 if __name__ == "__main__":
     it_item_index_id_mapping = get_it_item_index_id_mapping()
@@ -185,12 +86,6 @@ if __name__ == "__main__":
 
         print('score: ', score)
 
-        knowledge_str_list = read_file_content(int(id), ['knowledge', 'auto_knowledge'], True)
-        knowledge_id_list = []
-        for knowledge_str in knowledge_str_list:
-            knowledge_id_list.append(knowledge_index_map[knowledge_str])
-        # 写item
-        # item_file.append(str(id) + ',' + json.dumps(knowledge_id_list))
         score_file.append('1,' + str(id) + ',' + str(score))
 
         total_score += score
